03085f83c169d843ae24ccafd001554ace16cde7
Adam Byrne <adamrbyrne@gmail.com>
2023-03-06 11:38:28 +0000

    refac

--- app.py
+++ app.py
@@ -473,7 +473,5 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-
-
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
